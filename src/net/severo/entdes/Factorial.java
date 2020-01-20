52b5d2017f4506dad085bf3a2b8f2e23f3a73a76
Jaime Sansano García <jasano2@hotmail.com>
2020-01-20 11:36:10 +0100

    textos en alemán

--- src/net/severo/entdes/Factorial.java
+++ src/net/severo/entdes/Factorial.java
@@ -6,8 +6,8 @@ package net.severo.entdes;
  */
 public class Factorial {
 
-	private static final String VALUE_OF_N2 = "Value of n!: ";
-	private static final String VALUE_OF_N = "Value of n: ";
+	private static final String VALUE_OF_N2 = "Wert von n!: ";
+	private static final String VALUE_OF_N = "Wert von n: ";
 
 	public static void main(String[] args) {
 
